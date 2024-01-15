69539d66cc46732baa3d81ae0eedeba2866dc5d4
truongdc-3155 <104975435+truongdc-3155@users.noreply.github.com>
2024-01-15 21:17:05 +0700

    [Flavor] Create Platform channel for IOS.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,34 @@
+
 import UIKit
 import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  let platformChannel = "environment_flutter_channel"
+  let getFlavor = "flavor_method"
+
   override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+      // check and get native data from flutter
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let flutterChannel = FlutterMethodChannel(name: self.platformChannel, binaryMessenger: controller.binaryMessenger)
+      flutterChannel.setMethodCallHandler({
+          (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+          switch call.method {
+          case self.getFlavor:
+              let flavor = Bundle.main.infoDictionary?["AppFlavor"]
+              result(flavor)
+          default:
+              result(FlutterMethodNotImplemented)
+              return
+          }
+      })
+
+      GeneratedPluginRegistrant.register(with: self)
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+    
